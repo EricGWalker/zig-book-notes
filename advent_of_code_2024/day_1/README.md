35b0c7e51660f5a934bf3467c76a74823edeeac0
EricGWalker <ewalkerpriority@gmail.com>
2025-01-31 01:46:03 -0500

    eric is a big dumb dumb

---
+++ advent_of_code_2024/day_1/README.md
@@ -0,0 +1,8 @@
+# Advent of Code Day 1 
+
+Problem essentially boils down to:
+
+- Take a text input with 2 columns of numbers.
+- Build 2 min heaps
+- Take the difference between the two minimums, add that to a total, and then pop both minimum's off the heap.
+
---
+++ advent_of_code_2024/day_1/quicksort.zig
@@ -0,0 +1,35 @@
+//! Currently broken b/c I am a dumb dumb
+const std = @import("std");
+//const stdio = std.io.getStdOut().writer();
+const print = std.debug.print;
+
+fn quicksort(slice: []u32) void {
+    if (slice.len <= 1) {
+        return;
+    }
+
+    const pivot = slice[slice.len - 1];
+
+    var left_pivot: usize = 0;
+    for (0..slice.len) |right_pivot| {
+        if (slice[right_pivot] > pivot) {
+            continue;
+        } else if (slice[right_pivot] <= pivot) {
+            const new_right_pivot: u32 = slice[left_pivot];
+            slice[left_pivot] = slice[right_pivot];
+            slice[right_pivot] = new_right_pivot;
+            left_pivot += 1;
+        }
+    }
+
+    //left_pivot should be the final position of the pivot at the end of the loop
+    quicksort(slice[0..left_pivot]);
+    quicksort(slice[left_pivot + 1 ..]);
+}
+
+pub fn main() void {
+    var array = [_]u32{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    print("{any}\n", .{array});
+    quicksort(&array);
+    print("{any}\n", .{array});
+}
