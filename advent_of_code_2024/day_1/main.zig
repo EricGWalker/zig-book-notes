910fe0ee978b87064781a8f5dfbb5f31c676776c
EricGWalker <ewalkerpriority@gmail.com>
2025-02-02 01:53:41 -0500

    saving progress, starting to understand allocators and buffers

--- advent_of_code_2024/day_1/main.zig
+++ advent_of_code_2024/day_1/main.zig
@@ -1,13 +1,18 @@
 const std = @import("std");
 const quicksort = @import("quicksort.zig").quicksort;
 const stdout = std.io.getStdOut().writer();
+const test_allocator = std.testing.allocator;
 
 pub fn main() !void {
-    var array = [_]u32{ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-    try stdout.print("{any}\n", .{array});
-    quicksort(&array);
-    try stdout.print("{any}\n", .{array});
-
     const puzzle_input = try std.fs.cwd().openFile("puzzle_input.txt", .{});
     defer puzzle_input.close();
+
+    var buffered_reader = std.io.bufferedReader(puzzle_input.reader());
+
+    var content_buffer: [14]u8 = undefined; //size is 14 b/c each line of puzzle_input has 14 chars.
+    @memset(content_buffer[0..], 0);
+
+    const contents = try buffered_reader.reader().readUntilDelimiterOrEof(content_buffer[0..], '\n');
+
+    try stdout.print("{?s}", .{contents});
 }
